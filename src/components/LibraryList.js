c79b2dd78ac0bc6bcbbe57809f003f369c139874
Arthur BABEY <arthur.babey@icloud.com>
2017-08-17 19:39:31 +0200

    Lecture 88 : The connect function

---
+++ src/components/LibraryList.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class LibraryList extends Component {
+
+  constructor() {
+    super();
+  }
+
+  render() {
+  }
+}
+
+export default connect()(LibraryList);
--- src/reducers/LibraryReducer.js
+++ src/reducers/LibraryReducer.js
@@ -1,2 +1,3 @@
-export default () => [
-];
+import data from './LibraryList.json';
+
+export default () => data;
